50fc609677ae358b8ba7ab0bba7d2b53ef996ce8
Brian Hulbert <Brian.hulbert@me.com>
2019-05-28 19:13:04 -0400

    completed tetris game

--- Tetris.java
+++ Tetris.java
@@ -7,6 +7,10 @@ public class Tetris extends JComponent{
     int[] fbPos;
     boolean movingRight;
     boolean movingLeft;
+    double[] choices;
+    int score;
+    boolean isAlive;
+    int count = 1;
     /*
      * {1}, {1,1},                               {1,1}
      * {1}, {0,1}, {1,1} {1,1,1} {1,1,0} {0,1,1} {1,0}
@@ -22,28 +26,38 @@ public class Tetris extends JComponent{
         fbPos = new int[]{9,0};
         movingRight = false;
         movingLeft = false;
+        //0 == right, 1 == left, 2 == rotate right, 3 = rotate left
+        choices = new double[4];
+        score = 0;
+        isAlive = true;
     }
     public void draw(){
         super.repaint();
     }
     public void update(){
-        rotateLeft();
-        if(collided()){
-            rotateLeft();
-        }
-        if(movingRight){
+        if(choices[0] > 0.5){
             fbPos[0]++;
             if(collided()){
                 fbPos[0]--;
             }
-            movingRight = false;
-        }
-        if(movingLeft){
+        }   
+        if(choices[1] > 0.5){
             fbPos[0]--;
             if(collided()){
                 fbPos[0]++;
             }
-            movingLeft = false;
+        }
+        if(choices[2] > 0.5){
+            rotateRight();
+            if(collided()){
+                rotateLeft();
+            }   
+        }
+        if(choices[3] > 0.5){
+            rotateLeft();
+            if(collided()){
+                rotateRight();
+            }
         }
         fbPos[1]++;
         if(collided()){
@@ -57,7 +71,47 @@ public class Tetris extends JComponent{
             }
             fbPos = new int[]{9,0};
             fallingBlock = getShape();
+            count++;
+            if(collided()){
+                isAlive = false;
+            }
         }
+        int row = checkRow();
+        while(row != -1){
+            score += 10;
+            dropAllBlocks(row);
+            row = checkRow();
+        }
+    }
+    public void dropAllBlocks(int r){
+        for(int x = 0; x < board.length; x++){
+            board[x][r] = 0;
+        }
+        /*
+        for(int[] x : board){
+            for(int y: x){
+                System.out.print(y+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------------------");
+        */
+        if(r > 0){
+            for(int y = r-1; y >= 0; y--){
+                for(int x = 0; x < board.length; x++){
+                    board[x][y+1] = board[x][y];
+                }
+            }
+        }
+        /*
+        for(int[] x : board){
+            for(int y: x){
+                System.out.print(y+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------------------");
+        */
     }
     public void rotateRight(){
         int[][] newBlock = new int[fallingBlock[0].length][fallingBlock.length];
@@ -91,6 +145,21 @@ public class Tetris extends JComponent{
         }
         return false;
     }
+    public int checkRow(){
+        for(int y = 0; y < board[0].length; y++){
+            boolean found = true;
+            for(int x = 0; x < board.length; x++){
+                if(board[x][y] == 0){
+                    found = false;
+                }
+            }
+            if(found){
+                //System.out.println("Row: "+y);
+                return y;
+            }
+        }
+        return -1;
+    }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.setColor(Color.GRAY);
@@ -111,12 +180,15 @@ public class Tetris extends JComponent{
                 if(fallingBlock[x][y] ==  1){
                     g.fillRect((fbPos[0]+1+x)*20,(fbPos[1]+y)*20,20,20);
                 }
-                System.out.print(fallingBlock[x][y]+" ");
+                //  System.out.print(fallingBlock[x][y]+" ");
             }
-            System.out.println();
+            //System.out.println();
         }
-        System.out.println("("+fbPos[0]+","+fbPos[1]+")");
-        System.out.println("----------------------------");
+        g.setColor(Color.WHITE);
+        g.drawString("Score: "+score,25,15);
+        g.drawString("isAlive: "+isAlive,25,30);
+        //System.out.println("("+fbPos[0]+","+fbPos[1]+")");
+        //System.out.println("----------------------------");
     }
     public int[][] getShape(){
         return blocks[(int)(Math.random()*blocks.length)];
--- tetrisTester.java
+++ tetrisTester.java
@@ -10,9 +10,22 @@ public class tetrisTester{
         frame.setVisible(true);
         while(true){
             game.draw();
+            game.choices = new double[]{Math.random(),Math.random(),Math.random(),Math.random()};
             game.update();
+            /*
+            if(game.count%10 == 0){
+                for(int x = 0; x < game.board.length; x++){
+                    game.board[x][47] = 1;
+                    game.board[x][48] = 1;
+                }
+                game.count++;
+            }
+            */
+            if(!game.isAlive){
+                game.reset();
+            }
             try{
-                Thread.sleep(200);
+                Thread.sleep(50);
             }
             catch(Exception e){
                 System.out.println(e);
